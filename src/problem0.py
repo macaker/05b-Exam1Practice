381fcacc1432dc286902d44c0db3cb842127c99c
macaker <macaker@rose-hulman.edu>
2019-02-03 16:47:59 -0500

    exam practice

--- src/problem0.py
+++ src/problem0.py
@@ -189,7 +189,7 @@ def problem0a(n):
            since (2 + 4 + 6) is 12, which is NOT odd.
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ###########################################################################
@@ -206,16 +206,12 @@ def problem0a(n):
     #        Simply try a few examples to convince yourself of this.
     #        ASK FOR HELP if you do not understand this hint.
     # -------------------------------------------------------------------------
-   for k in range(n):
-        n = sum_of_digits(n)
-        if k % 2== 0:
-
-
-
-        count = count +1
+    m = sum_of_digits(n)
+    if m % 2 == 1:
         return True
     else:
         return False
+
 
 def run_test_problem0b():
     """ Tests the   problem0b   function. """
@@ -271,7 +267,7 @@ def problem0b(n):
            since there are 46 primes between 2 and 200.
      """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ###########################################################################
@@ -280,6 +276,11 @@ def problem0b(n):
     #    **  use (call) the   is_prime   function that is DEFINED ABOVE.
     ###########################################################################
     # ------------------------------------------------------------------
+    count = 0
+    for k in range(2, n+1):
+        if is_prime(k):
+            count = count +1
+    return count
 
 
 def run_test_problem0c():
@@ -339,7 +340,7 @@ def problem0c(circle, n, window):
       :type window: rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ###########################################################################
@@ -347,7 +348,12 @@ def problem0c(circle, n, window):
     #   renders with a half-second pause after rendering.
     ###########################################################################
     # -------------------------------------------------------------------------
-
+    circle.attach_to(window)
+    for k in range(n):
+        circle = rg.Circle(circle.center, circle.radius)
+        circle.move_by(circle.radius *2, 0)
+        circle.attach_to(window)
+        window.render(.5)
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
